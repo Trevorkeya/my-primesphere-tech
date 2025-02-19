ad41e5c9acf89923cad12dc762181c818e66a319
Trevorkeya <trevor.anjere@strathmore.edu>
2025-02-19 19:26:19 +0300

    finishing order views and templates

--- Ecom/payment/views.py
+++ Ecom/payment/views.py
@@ -8,15 +8,181 @@ from store.models import Product, Profile
 import datetime
 
 # Create your views here.
+def orders(request, pk):
+    if request.user.is_authenticated and request.user.is_superuser:
+        # Get the order
+        order = Order.objects.get(id=pk)
+        # Get the order items
+        items = OrderItem.objects.filter(order_id=pk)
+
+        if request.POST:
+            status = request.POST['shipping_status']
+            # Check If True Or False 
+            if status == 'true':
+                # Get The Order
+                order = Order.objects.filter(id=pk)
+                # Update The Status
+                now = datetime.datetime.now()
+                order.update(shipped=True, date_shipped = now)
+            else:
+                # Get The Order
+                order = Order.objects.filter(id=pk)
+                # Update The Status
+                order.update(shipped=False)
+            messages.success(request, 'Shipping Status Updated.')
+            return redirect('home')
+
+        return render(request, 'payment/orders.html', {'order':order, 'items':items})
+       
+    else:
+        messages.warning(request, "Access Denied! ! !")
+        return redirect('home')
+
+def not_shipped_dash(request):
+    if request.user.is_authenticated and request.user.is_superuser:
+        orders = Order.objects.filter(shipped=False)
+
+        if request.POST:
+            status = request.POST['shipping_status']
+            num = request.POST['num']
+            # Get the Order
+            order = Order.objects.filter(id=num)
+            
+            # Grab Date and Time
+            now = datetime.datetime.now()
+            order.update(shipped=True, date_shipped = now)
+
+            messages.success(request, 'Shipping Status Updated.')
+            return redirect('shipped_dash')
+
+        return render(request, 'payment/not_shipped_dash.html', {'orders':orders})
+    else:
+        messages.warning(request, "Access Denied! ! !")
+        return redirect('home')
+
+
+def shipped_dash(request):
+    if request.user.is_authenticated and request.user.is_superuser:
+        orders = Order.objects.filter(shipped=True)
+
+        if request.POST:
+            status = request.POST['shipping_status']
+            num = request.POST['num']
+            # Get the Order
+            order = Order.objects.filter(id=num)
+
+            # Grab Date and Time
+            now = datetime.datetime.now()
+            order.update(shipped=False)
+
+            messages.success(request, 'Shipping Status Updated.')
+            return redirect('not_shipped_dash')
+
+        return render(request, 'payment/shipped_dash.html', {'orders':orders})
+    else:
+        messages.warning(request, "Access Denied! ! !")
+        return redirect('home')
+
 
 def process_order(request):
     if request.POST:
+        # Get the Cart 
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+
         # Get Billing info from the last Page
         payment_form = PaymentForm(request.POST or None)
         # Get Shipping session Data
+        my_shipping = request.session.get('my_shipping')
+
+        # Gather order data
+        full_name = my_shipping['shipping_full_name']
+        email = my_shipping['shipping_email']
+        # Create shipping address from session data
+        shipping_address = f"{my_shipping['shipping_address1']}\n{my_shipping['shipping_address2']}\n{my_shipping['shipping_city']}\n{my_shipping['shipping_state']}\n{my_shipping['shipping_zipcode']}\n{my_shipping['shipping_country']}"
+        amount_paid = totals
+
+        # Create Order
+        if request.user.is_authenticated:
+            # Logged in user
+            user = request.user
+            # create order
+            create_order = Order(user=user, full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
+            create_order.save()
+
+            # Add Order Items
+
+            # Get the order ID
+            order_id = create_order.pk
+            # Get product info
+            for product in cart_products():
+                product_id = product.id #get product ID
+                # Get the product price
+                if product.is_sale:
+                    price = product.sale_price
+                else:
+                    price = product.price
+                # Get the quantity
+                for key, value in quantities().items():
+                    if int(key) == product.id:
+                        # Create Order Item
+                        create_order_item = OrderItem(order_id=order_id, product_id=product_id, user=user, quantity=value, price=price)
+                        create_order_item.save()
+                
+            # Clear the cart
+            for key in list(request.session.keys()):
+                if key == "session_key":
+                    # Delete the session key
+                    del request.session[key]
+
+            # Delete Cart from Database (old_cart field)
+            current_user = Profile.objects.filter(user__id=request.user.id)
+            # Delete Shopping Cart In Database (Old Cart Field)
+            current_user.update(old_cart="")
+
+            messages.success(request, "Order Placed!")
+            return redirect('home')
+
+        else:
+            # not logged in
+            # create order
+            create_order = Order(full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
+            create_order.save()
+
+             # Add Order Items
+
+            # Get the order ID
+            order_id = create_order.pk
+            # Get product info
+            for product in cart_products():
+                product_id = product.id #get product ID
+                # Get the product price
+                if product.is_sale:
+                    price = product.sale_price
+                else:
+                    price = product.price
+                # Get the quantity
+                for key, value in quantities().items():
+                    if int(key) == product.id:
+                        # Create Order Item
+                        create_order_item = OrderItem(order_id=order_id, product_id=product_id, quantity=value, price=price)
+                        create_order_item.save()
+
+            # Clear the cart
+            for key in list(request.session.keys()):
+                if key == "session_key":
+                    # Delete the session key
+                    del request.session[key]
+
+            messages.success(request, "Order Placed!")
+            return redirect('home')
+
     else:
         messages.warning(request, "Access Denied! ! !")
         return redirect('home') 
+
 
 def billing_info(request):
     if request.POST:
@@ -25,6 +191,10 @@ def billing_info(request):
         cart_products = cart.get_prods
         quantities = cart.get_quants
         totals = cart.cart_total()
+
+        # create a session with shipping info
+        my_shipping = request.POST
+        request.session['my_shipping'] = my_shipping
 
         # check to see if user is logged in
         if request.user.is_authenticated:
@@ -65,6 +235,7 @@ def checkout(request):
         # Checkout as Guest 
         shipping_form = ShippingForm(request.POST or None)
         return render(request, 'payment/checkout.html', {'cart_products':cart_products, 'quantities':quantities, 'totals':totals, 'shipping_form':shipping_form})
+
 
 def payment_success(request):
     return render(request, 'payment/payment_success.html', {})
