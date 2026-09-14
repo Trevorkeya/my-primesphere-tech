843773f56c4b577826025311971b7e1291a08786
Trevorkeya <107400255+Trevorkeya@users.noreply.github.com>
2026-09-14 16:22:12 +0000

    first Codespaces use

--- Ecom/Ecom/settings.py
+++ Ecom/Ecom/settings.py
@@ -13,7 +13,11 @@ SECRET_KEY = 'django-insecure-ow7a4b-0x&@!68f)b$dynsl1(y1kkx&k770t1nccg$8bv*z#2!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://localhost:8000",
+]
 
 
 # Application definition
